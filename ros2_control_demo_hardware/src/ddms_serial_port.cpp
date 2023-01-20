773e79afd91aed8eada067de9d0242a52ea0608f
mcg <m@m.m>
2023-01-19 19:26:55 -0500

    serial working crash on move

--- ros2_control_demo_hardware/src/ddms_serial_port.cpp
+++ ros2_control_demo_hardware/src/ddms_serial_port.cpp
@@ -42,14 +42,10 @@ ddms_diff::return_type DDMSSerial::open(const std::string & port_name)
     }
 
     memset(&tty_config, 0, sizeof(termios));
-    tty_config.c_cflag = B9600 | CRTSCTS | CS8 | CLOCAL | CREAD;
-    tty_config.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
-    tty_config.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-    tty_config.c_cflag &= ~CSIZE;    // Clear bits per byte
-    tty_config.c_cflag |=  CS8;      // 8 bit per byte
-    tty_config.c_iflag = IGNPAR;
-    tty_config.c_oflag = OPOST;
-    tty_config.c_lflag = 0;
+    tty_config.c_cflag = B115200 ;//| CRTSCTS | CS8 | CLOCAL | CREAD;
+    tty_config.c_iflag &= ~(INLCR | IGNCR | ICRNL | IXON | IXOFF);
+    tty_config.c_oflag &= ~(ONLCR | OCRNL);
+    tty_config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
     tty_config.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
     tty_config.c_cc[VMIN] = 0;
     tcflush(serial_port_, TCIFLUSH);
@@ -79,6 +75,29 @@ ddms_diff::return_type DDMSSerial::close()
     }
     return return_type::SUCCESS;
 }
+
+
+ddms_diff::return_type DDMSSerial::get_wheel_state(uint8_t ID,double velocity,std::vector<double> & states)
+{
+    //write velocity to motor
+    return_type retval = motor_command(ID,velocity);
+
+    if(retval == return_type::SUCCESS){
+        command_motor_reply reply;
+        if((retval = read_frame((uint8_t * )(&reply))) == return_type::SUCCESS){
+            //velocity 
+            states.push_back(((reply.velocity[0] << 8 )+ reply.velocity[1])/(double)32767);
+            //position
+            states.push_back((((reply.position[0] << 8) + reply.position[1])/(double)32767 )* 2* M_PI);
+        }else{
+            return retval;
+        }
+    }else{
+        RCLCPP_ERROR(rclcpp::get_logger("DDMSSerialPort"), "Failed to read state ID:%i, error %i",ID,(uint)retval);
+        return retval;
+    }
+    return ddms_diff::return_type::SUCCESS;
+}
 return_type DDMSSerial::motor_command(uint8_t ID, double commanded_value)
 {
     //write velocity to motor
@@ -87,42 +106,30 @@ return_type DDMSSerial::motor_command(uint8_t ID, double commanded_value)
     int16_t integer_velocity = (int)32767/commanded_value;
     command.commanded_state[0] = integer_velocity >>8;
     command.commanded_state[1] = integer_velocity;
+    //RCLCPP_INFO(rclcpp::get_logger("DDMSSerialPort"),"command %d %d %d", command.ID,command.commanded_state[0],command.commanded_state[1]);
+
     return write_frame((uint8_t *)(&command));
 }
-
 return_type DDMSSerial::read_frame(uint8_t * frame)
 {
     // Read data from the serial port
-    const ssize_t num_bytes = ::read(serial_port_, rx_buffer_, 200);
-    if (num_bytes == -1) {
+    ssize_t num_bytes=0,retval=0;
+    uint8_t rx_buffer[20];
+    do{
+        retval = ::read(serial_port_, rx_buffer, 20);
+        num_bytes+= retval;
+
+    }while(retval > -1 && num_bytes < 10);
+
+    //if the port didn't respond (or incomplete) we have a problem
+    if (num_bytes < 10) {
         RCLCPP_ERROR(rclcpp::get_logger("DDMSSerialPort"),"Failed to read serial port data: %s (%d)\n", strerror(errno), errno);
         return return_type::ERROR;
     }
-    if(num_bytes >=9){
-        memcpy(frame,rx_buffer_,9);
-        return return_type::SUCCESS;
-    }
-    return return_type::ERROR;
+    memcpy(frame,rx_buffer,10);
+    return return_type::SUCCESS;
 }
-std::vector<double> DDMSSerial::get_wheel_state(uint8_t ID,double velocity)
-{
-        //write velocity to motor
-        std::vector<double> states;
-        return_type retval = motor_command(ID,velocity);
-    if(retval == return_type::SUCCESS){
-        command_motor_reply reply;
-        if(read_frame((uint8_t * )(&reply)) == return_type::SUCCESS){
-            //velocity 
-            states.push_back(((reply.velocity[0] << 8 )+ reply.velocity[1])/(double)32767);
-            //position
-            states.push_back((((reply.position[0] << 8) + reply.position[1])/(double)32767 )* 2* M_PI);
-        }
-    }else{
-          RCLCPP_ERROR(rclcpp::get_logger("DDMSSerialPort"), "Failed to read state ID:%i, error %i",ID,(uint)retval);
 
-    }
-    return states;
-}
 
 ddms_diff::return_type DDMSSerial::write_frame(uint8_t* data)
 {
@@ -149,20 +156,11 @@ bool DDMSSerial::is_open() const
 // CRC-8/MAXIM
 uint8_t DDMSSerial::crc_update(const uint8_t* data)
 {
-    uint8_t crc, i;
-    crc = 0x00;
-    i=0;
-    while(i < DDMS_SERIAL_SERIAL_FRAME_SIZE - 1)//fixed frame size
-    {
-        crc = crc ^ *data++;
-        for(i = 0;i < 9;i++)
-        {
-            if(crc & 0x01)
-            {
-                crc = (crc >> 1) ^ 0x8c;
-            }
-                else crc >>= 1;
-        }
+    unsigned crc = 0;
+    for (size_t i = 0; i < DDMS_SERIAL_SERIAL_FRAME_SIZE-1; i++) {
+        crc ^= data[i];
+        for (unsigned k = 0; k < 8; k++)
+            crc = crc & 1 ? (crc >> 1) ^ 0x8c : crc >> 1;
     }
     return crc;
 }
